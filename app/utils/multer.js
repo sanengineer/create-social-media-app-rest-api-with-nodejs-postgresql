2cf60050ba932be697e756423633b091bddada58
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-01-27 22:26:40 +0700

    update

--- app/utils/multer.js
+++ app/utils/multer.js
@@ -1,12 +1,25 @@
 const multer = require("multer");
 const path = require("path");
+
+console.log("PATTTHH:", path.extname);
 // Multer config
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
+    //
+    //debug
+    console.log("EXTTT:", ext);
+
+    if (
+      ext !== ".jpg" &&
+      ext !== ".jpeg" &&
+      ext !== ".png" &&
+      ext !== ".JPG" &&
+      ext !== ".PNG" &&
+      ext !== ".JPEG"
+    ) {
+      cb(new Error("Srorry Your File type is not supported"), false);
       return;
     }
     cb(null, true);
